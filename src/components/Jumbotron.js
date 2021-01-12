eac71c2976379a6aec4ea461f1e37f91348fb8e3
Thom Rattana <thomr95@gmail.com>
2021-01-11 21:49:02 -0500

    set basic template

---
+++ src/components/Jumbotron.js
@@ -0,0 +1,15 @@
+import React from "react";
+import "../styles/Jumbotron.css";
+
+
+function Jumbotron() {
+  return (
+    <div className="jumbotron mb-0 px-3 py-5 rounded-0">
+      <div className="jumbotron-info border rounded p-2">
+        <h1 className="display-4">Employee Directory</h1>
+      </div>
+    </div>
+  );
+}
+
+export default Jumbotron;
